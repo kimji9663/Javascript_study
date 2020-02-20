86289ab97b2622a9d24c134901ba15d707ce995b
kimji9663 <rlawlgp9663@naver.com>
2020-02-21 02:34:01 +0900

    Modify 'marathon' algorithm

--- algorithm/marathon.js
+++ algorithm/marathon.js
@@ -4,12 +4,13 @@
 - 조건3 : 이름 = 1 < 20, lowercase
 */
 
-function solution(participant, completion) {
+function solution(participant, completion) { //참가자 수, 완주한 사람 수를 인수로 받음
     
-    participant.sort();
-    completion.sort();
+    participant.sort(); //참가자 수를 알파벳 순서대로 배열을 반환한다.
+    completion.sort(); //완주한 사람 수를 알파벳 순서대로 배열을 반환한다.
     
-    for(let i in participant) {
-        if(participant[i] !== completion[i]) return participant[i];
+    for(let i in participant) { //속성 in 객체명 :  in 연산자는 명시된 속성이 명시된 객체에 존재하면 true를 반환
+        participant[i] = participant[i].toLowerCase(); //참가자 이름을 모두 소문자로 변환
+        if(participant[i] !== completion[i]) return participant[i]; //참가자와 완주한 사람이 일치하지 않으면 그 값을 반환
     }
 }
