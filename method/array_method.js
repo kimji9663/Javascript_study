88f2872fefa5ff561d201816b0e0201f041f4624
kimji9663 <rlawlgp9663@naver.com>
2022-02-11 01:54:22 +0900

    배열 메서드 예제 풀기

--- method/array_method.js
+++ method/array_method.js
@@ -31,3 +31,24 @@ const numbers2 = [ 12, 4, 23, 7 ];
 const result3 = numbers2.filter( number => number < 10 );
 
 console.log(result3);
+
+
+
+
+// 4. 복사하기
+const numbers4 = [ 2, 4, 3, 7 ];
+
+// concat
+const result4_1 = numbers4.concat();
+console.log(result4_1);
+
+// slice
+const result4_2 = numbers4.slice();
+console.log(result4_2);
+
+// reduce
+let result4_3 = [];
+numbers4.reduce((acc, cur) => {	
+    return result4_3.push(cur);
+}, result4_3);
+console.log(result4_3);
