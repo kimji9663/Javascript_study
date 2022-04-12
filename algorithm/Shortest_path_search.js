d1cad76a9ff7dd72bc77dace484b8f5191054ea9
kimji9663 <rlawlgp9663@naver.com>
2022-04-13 01:06:50 +0900

    [알고리즘] 최단 거리 탐색 다시풀기

--- algorithm/Shortest_path_search.js
+++ algorithm/Shortest_path_search.js
@@ -1,40 +1,37 @@
 // 최단 경로 탐색
 
-graph = {'A': ['B', 'C'],
+graph = {
+         'A': ['B', 'C'],
          'B': ['A', 'D', 'E'],
          'C': ['A', 'F'],
          'D': ['B'],
          'E': ['B', 'F'],
-         'F': ['C', 'E']}
+         'F': ['C', 'E']
+        }
 
 // 입력 : A F
 
-let user_input = ['A', 'F'];
-const start = user_input[0];
-const end = user_input[1];
-
-let queue = [start];
-let visited = [start];
-
-function solution() {
-    let count = -1;
-
-    while(queue.length !== 0){
-        count += 1;
-        let size = queue.length;
-        
-        for (let i = 0; i < size; i++){
-            let node = queue.splice(0, 1);
-            if (node == end){
-                return count;
+function sps(start, end) {
+    let visited = [start];
+    let queue = [start];
+  	let count = -1;
+    while (queue.length !== 0){
+      	count += 1;
+      	let size = queue.length;
+      	for(let i = 0; i < size; i++){
+          let n = queue.splice(0, 1);
+          if (n == end){
+            return count;
+          }
+          for (let next_node in graph[n]){
+            if(!visited.includes(graph[n][next_node])){
+              visited.push(graph[n][next_node]);
+              queue.push(graph[n][next_node]);
             }
-            for (let next_node in graph[node]){
-                if(!visited.includes(graph[node][next_node])){
-                    visited.push(graph[node][next_node]);
-                    queue.push(graph[node][next_node]);
-                }
-            }
+          }
         }
     }
+    return visited;
 }
-console.log(solution());
+
+sps('A', 'F');
